31350e0f820b632597ff6770fa0ceb04637c8b2c
vasuenduri <srinivas06239@gmail.com>
2022-11-02 18:00:10 +0530

    Update update_script.py

--- update_script.py
+++ update_script.py
@@ -1,2 +1,14 @@
 import yaml
-print("I have yaml")
+import sys
+
+# Load the file into a data structure
+with open(sys.argv[1], "r") as file:
+    content = yaml.safe_load(file)
+
+# Update the key you want to change
+#content["spec"]["resources"]["requests"]["storage"] = sys.argv[2]
+content["items"]["spec"]["template"]["containers"]["image"] = sys.argv[2]
+
+# Write the data structure back to your file in YAML
+with open(sys.argv[1], "w") as file:
+    yaml.safe_dump(content, file)
